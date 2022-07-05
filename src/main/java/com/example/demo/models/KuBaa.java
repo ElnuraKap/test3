839cc7a83c1e45ada4de02d48419ea803a89e4c1
User <abdurasilov99@gmail.com>
2022-07-05 21:11:36 +0600

    I do open the new class and new branch.

---
+++ src/main/java/com/example/demo/models/KuBaa.java
@@ -0,0 +1,12 @@
+package com.example.demo.models;
+
+/**
+ * from in Kadamjay
+ */
+public class KuBaa {
+
+    private String Batken;
+    private String kadamjay;
+    private String Shkolnaya;
+    private int nomer47;
+}
