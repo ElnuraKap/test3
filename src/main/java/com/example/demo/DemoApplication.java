ff2fd8a2beabf3da7881ff2157c8bea9b244f0ab
nargiza <sultanmuratova500@gmail.com>
2022-07-05 20:44:34 +0600

    fdsf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,34 +4,15 @@ import com.example.demo.models.Programmer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        System.out.println("Nasgiza's conflict");
-        System.out.println("dewklj");
-
-
-        System.out.println("nargiza");
-        System.out.println("mkdslfklds");
-
-
-        System.out.println("Hello java 5");
-
-
-        int i = 9;
-        for(int j = 0; j < i; j++) {
-            System.out.println("Dilbara's code");
-        }
-
-
-        System.out.println("mukhammed");
-
-
-        System.out.println("DILBARA");
-
+        ArrayList<Integer> arrayList=new ArrayList();
 
     }
 
