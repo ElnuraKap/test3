a0f3f43770fa80939b349412816400d7db923220
Elnura Kaparova <98158684+ElnuraKap@users.noreply.github.com>
2022-07-05 20:10:21 +0600

    Revert "Nargiza"

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        System.out.println("nargiza");
-        System.out.println("mkdslfkldsjf");
-
+        System.out.println("Hello java 5");
     }
 
 }
