7c3fedfba1f52b7b7de493bcd14067d4898be7ff
nargiza <sultanmuratova500@gmail.com>
2022-07-05 20:20:03 +0600

    fmdks

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,17 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
-        System.out.println("nargiza");
-        System.out.println("mkdslfkldsjf");
-
-        System.out.println("Hello java 5");
-
-
-        int i = 9;
-        for(int j = 0; j < i; j++) {
-            System.out.println("Dilbara's code");
-        }
+        System.out.println("Nasgiza's conflict");
 
 
 
