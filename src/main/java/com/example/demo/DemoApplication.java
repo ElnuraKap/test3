ca6484f1de9dc916ba6e0138598580a8c5305fe0
Muhammed <muhammedtoichubai@gmail.com>
2022-07-05 07:55:17 -0600

    muhammed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,6 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-
         Programmer muhammed = new Programmer("Muhammed", 25);
         System.out.println("Hello java 5");
 
