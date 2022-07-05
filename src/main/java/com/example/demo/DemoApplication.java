a578d2464b43e9e5305ec687e06c7d12d294e3e6
Dilbara Asanalieva <dilbara2025@gmail.com>
2022-07-05 19:52:29 +0600

    1st commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,11 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        System.out.println("nargiza");
-        System.out.println("mkdslfkldsjf");
-        System.out.println("Hello java 5");
+        int i = 5;
+        for(int j = 0; j < i; j++) {
+            System.out.println("Dilbara's code");
+        }
+
     }
 
 }
