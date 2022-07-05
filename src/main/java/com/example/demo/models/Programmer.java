2534c026d737be8bba9f99765d55703dbfe41fde
Muhammed <muhammedtoichubai@gmail.com>
2022-07-05 07:48:38 -0600

    commit muhammed

---
+++ src/main/java/com/example/demo/models/Programmer.java
@@ -0,0 +1,20 @@
+package com.example.demo.models;
+
+/**
+ * @author Muhammed Toichubai
+ */
+
+public class Programmer {
+
+    private String name;
+
+    private Integer age;
+
+    public Programmer() {
+    }
+
+    public Programmer(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+}
