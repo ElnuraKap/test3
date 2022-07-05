67401a5253ddf52e58119f96bd956e0e463ea6db
nargiza <sultanmuratova500@gmail.com>
2022-07-05 20:35:42 +0600

    fdsf111

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
         System.out.println("Nasgiza's conflict");
+        System.out.println("dewklj");
 
 
         System.out.println("nargiza");
